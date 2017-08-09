5b0da07cfedf892d33f5c6733330e64d85295a9a
duyet.vo <duyet.vo@sutrixsolutions.com>
2017-08-09 19:08:51 +0700

    [update]

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
 window.game = {
-  timeperframe: 60,
-  vx: 10,
-  vy: 8
+  tpf: 60,
+  vx : 10,
+  vy : 8,
+
+  createImage: function(src) {
+    var img = new Image();
+    img.src = src;
+    return img;
+  }
 };
 
-// ---------------------------------------------
+window.game.entity = {
+  coin: {
+    image       : window.game.createImage('sprite.png'),
+    spriteStart : 0,
+    spriteEnd   : 9,
+    spriteWidth : 100,
+    spriteHeight: 100,
+    spritePerRow: 10,
+    posX        : 200,
+    posY        : 200
+  }
+};
+
+// ______________________________________________________
+//                                                 SPRITE
+// ------------------------------------------------------
 (function (game) {
 
-  function Sprite (image, start, end, width, height, spritePerRow) {
-    this.image        = image;
-    this.start        = start;
-    this.end          = end;
-    this.width        = width;
-    this.height       = height;
-    this.spritePerRow = spritePerRow;
-    this.main();
+  function Sprite (opts) {
+    this.image        = opts.image;
+    this.start        = opts.start;
+    this.end          = opts.end;
+    this.spritePerRow = opts.spritePerRow;
+
   }
 
   Sprite.prototype = {
@@ -22,12 +41,7 @@ window.game = {
     props: {
       x   : 0,
       y   : 0,
-      curr: this.start,
-      isMove: false
-    },
-
-    main: function () {
-      console.log(this.image);
+      curr: this.start
     },
 
     init: function () {
@@ -36,42 +50,114 @@ window.game = {
     },
 
     update: function () {
-      if (!this.props.isMove) {
-        return;
-      }
 
       this.props.curr ++;
       (this.props.curr > this.end) && (this.props.curr = this.start);
 
+      this.init();
+
+      console.log('position ', this.props.x, this.props.y);
     },
 
-    move: function () {
-      this.props.isMove = true;
-    },
+    x: function() { return this.props.x; },
 
-    stop: function () {
-      this.props.isMove = false;
-    }
+    y: function () { return this.props.y; }
+
   }
 
   game.Sprite = Sprite;
 
 }(window.game));
 
-// -----------------------------------------------------
+// ___________________________________________________
+//                                         Game object
+  // (function (game){
 
+  //   function GameObject (image, spriteStart, spriteEnd, spriteWidth, spriteHeight, spritePerRow, posX, posY) {
+  //     this.spriteStart  = spriteStart;
+  //     this.spriteEnd    = spriteEnd,
+  //     this.spriteWidth  = spriteWidth;
+  //     this.spriteHeight = spriteHeight;
+  //     this.spritePerRow = spritePerRow;
+  //     this.image        = image;
+  //   }
+
+  //   GameObject.prototype = {
+
+  //     props: {
+  //       x: this.posX,
+  //       y: this.posY,
+  //       vx: 0,
+  //       vy: 0,
+  //       isMove: false
+  //     },
+
+  //     init: function () {
+  //       this.sprite = new game.Sprite(this.image, this.spriteStart, this.spriteEnd, this.spriteWidth, this.spriteHeight, this.spritePerRow);
+  //     },
+
+  //     update: function () {
+  //       if (!this.props.isMove) { return; }
+
+  //       this.props.x += this.props.vx;
+  //       this.props.y += this.props.vy;
+  //     },
+
+  //     moveLeft: function () {
+  //       this.props.vx = game.vx;
+  //     },
+
+  //     moveRight: function () {
+  //       this.props.vx = -game.vx;
+  //     },
+
+  //     moveDown: function () {
+  //       this.props.vy = game.vy;
+  //     },
+
+  //     moveUp: function () {
+  //       this.props.vy = -game.vy;
+  //     },
+
+  //     move: function () {
+  //       this.props.isMove = true;
+  //     },
+
+  //     stand: function () {
+  //       this.props.isMove = false;
+  //       this.props.vx = 0;
+  //       this.props.vy = 0;
+  //     }
+  //   }
+
+  //   game.GameObject = GameObject;
+  // }(window.game));
+
+// --------------------------------------------------
+
+
+// ___________________________________________________
+//                                         COIN OBJECT
+// ---------------------------------------------------
 (function (game){
 
-  function GameObject (image, spriteStart, spriteEnd, spriteWidth, spriteHeight, spritePerRow, posX, posY) {
-    this.spriteStart  = spriteStart;
-    this.spriteEnd    = spriteEnd,
-    this.spriteWidth  = spriteWidth;
-    this.spriteHeight = spriteHeight;
-    this.spritePerRow = spritePerRow;
-    this.image        = image;
+  function CoinObject (opts, context) {
+    this.context      = context;
+    this.image        = opts.image;
+
+    this.spriteStart  = opts.spriteStart;
+    this.spriteEnd    = opts.spriteEnd,
+    this.spriteWidth  = opts.spriteWidth;
+    this.spriteHeight = opts.spriteHeight;
+    this.spritePerRow = opts.spritePerRow;
+
+    this.posX         = opts.posX;
+    this.posY         = opts.posY;
+
+    this.init();
   }
 
-  GameObject.prototype = {
+  CoinObject.prototype = {
 
     props: {
       x: this.posX,
@@ -82,14 +168,19 @@ window.game = {
     },
 
     init: function () {
-      this.sprite = new game.Sprite(this.image, this.spriteStart, this.spriteEnd, this.spriteWidth, this.spriteHeight, this.spritePerRow);
+      this.sprite = new game.Sprite({image: this.image, start: this.spriteStart, end: this.spriteEnd, spritePerRow: this.spritePerRow});
     },
 
     update: function () {
-      if (!this.props.isMove) { return; }
+      // if (!this.props.isMove) { return; }
 
+      this.sprite.update();
       this.props.x += this.props.vx;
       this.props.y += this.props.vy;
+    },
+
+    render: function () {
+      this.context.drawImage(this.image, this.sprite.x(), this.sprite.y(), this.spriteWidth, this.spriteHeight, this.props.x, this.props.y, 100, 100);
     },
 
     moveLeft: function () {
@@ -119,18 +210,118 @@ window.game = {
     }
   }
 
-  game.GameObject = GameObject;
+  game.CoinObject = CoinObject;
 }(window.game));
 
 
-(function () {
+(function (game) {
 
-  function GamePlay () {
-
+  function GamePlay (context) {
+    this.context = context;
   }
 
   GamePlay.prototype = {
 
+    init: function () {
+      this.coin = new game.CoinObject(game.entity.coin, this.context);
+      console.log(this.coin);
+    },
+
+    update: function () {
+      this.coin.update();
+    },
+
+    render: function () {
+      this.coin.render();
+    }
   }
 
-}());
+  game.GamePlay = GamePlay;
+
+}(window.game));
+
+
+(function (game) {
+
+  var timestamp = function () {
+    return window.performance
+          && window.performance.now
+          ? window.performance.now()
+          : new Date().getTime();
+  };
+
+  function Game (canvasDiv) {
+    this.canvasDiv = canvasDiv;
+    this.main();
+  }
+
+  Game.prototype = {
+
+    props: {
+      lastTime: 0,
+      step: 1000 / game.tpf,
+      frameID: null
+    },
+
+    main: function () {
+      this.initWindow();
+      this.init();
+      this.run();
+    },
+
+    initWindow: function () {
+      this.canvas = document.createElement('canvas');
+
+      this.canvasDiv.appendChild(this.canvas);
+      this.canvas.setAttribute('width', this.canvasDiv.innerWidth);
+      this.canvas.setAttribute('height', this.canvasDiv.innerHeight);
+
+      this.context = this.canvas.getContext('2d');
+    },
+
+    init: function () {
+      this.gamePlay = new game.GamePlay(this.context);
+      this.gamePlay.init();
+    },
+
+    update: function (dt) {
+      this.gamePlay.update();
+    },
+
+    render: function (dt) {
+      this.gamePlay.render();
+    },
+
+    run: function () {
+      var that = this,
+          now  = 0,
+          dt   = 0,
+          last = timestamp(),
+          step = 1000 / 60;
+
+      function frame() {
+        now = timestamp();
+        dt  = dt + Math.min(1000, (now - last));
+
+        while(dt > step) {
+          dt = dt - step;
+          that.update(step);
+        }
+
+        that.render(dt);
+        last = now;
+        that.props.frameID = requestAnimationFrame(frame);
+      }
+
+      that.props.frameID = requestAnimationFrame(frame);
+    },
+
+    stop: function () {
+      cancelAnimationFrame(this.props.frameID);
+    }
+  }
+
+
+  new Game(document.getElementById('canvas-div'));
+
+}(window.game));
